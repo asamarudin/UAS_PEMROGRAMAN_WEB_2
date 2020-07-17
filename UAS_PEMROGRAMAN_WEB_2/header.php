f9719d66020ceb1b01a34995c18fa0b991bf025d
asmarudin <asmarudin0509@gmail.com>
2020-07-17 21:43:32 +0700

    publish file index

---
+++ UAS_PEMROGRAMAN_WEB_2/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CSS-->
+
+
+     <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/app.css" />
+    <link rel="stylesheet" href="assets/css/foundation.css" />
+
+</head>
+
+<body>
+<header>
+
+        <!--navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" style="margin-left:600px;" href="#">Data Relawan Covid-19</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+        <!--end Navbar-->
+    </header>
